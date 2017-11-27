023deec39104aa1ca8f8cc8fd24c2008eae2ff4b
illyay2017 <illya.yanchuk@gmail.com>
2017-11-27 12:33:24 -0800

    handles strings

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,26 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // handle array
+  // var result = [];
+  var output = '';
+  if (typeof obj === 'string') {
+    output += obj;
+  }
+
+  if (Array.isArray(obj)){
+    for (var i = 0; i < obj.length; i++){
+      result.push(stringifyJSON(obj[i]));
+    }
+    return result;
+  }
+  
+  return output;
+
+  // handle obj
+
+  // handle string   ''+'"xx"'  'xx'
+
+  // use + method
+
 };
