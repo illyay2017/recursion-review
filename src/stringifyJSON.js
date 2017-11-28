3e9a897e7442d4d77328d4994900bf5e9a58f208
illyay2017 <illya.yanchuk@gmail.com>
2017-11-27 16:19:18 -0800

    finished stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,22 +9,87 @@ var stringifyJSON = function(obj) {
   // var result = [];
   var output = '';
   if (typeof obj === 'string') {
-    output += obj;
+    output += '"' + obj + '"';
+    return output;
   }
-
-  if (Array.isArray(obj)){
+  if (Array.isArray(obj) && obj.length >= 0) {
+    output = [];
     for (var i = 0; i < obj.length; i++){
-      result.push(stringifyJSON(obj[i]));
+      output.push(stringifyJSON(obj[i]));
     }
-    return result;
+    return '[' + output + ']'; 
   }
+
+  if(obj === null) {
+    return '' + null;
+  }
+
+  if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+    // if (obj.length === undefined) {
+      return '{' + output + '}';
+    }
+    output = [];
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined'){
+        return '{}';
+      }
+      // output[stringifyJSON(key)] = stringifyJSON(obj[key]);
+      output.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    
+    }
+    return '{' + output + '}';
+  }
+
   
+//   // return obj.toString();
+  output = '' + obj;
   return output;
+}
+  
+// var stringifiableObjects = [
+//   9,
+//   null,
+//   true,
+//   false,
+//   'Hello world',
+//   [],
+//   [8],
+//   ['hi'],
+//   [8, 'hi'],
+//   [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+//   [8, [[], 3, 4]],
+//   [[[['foo']]]],
+//   {},
+//   {'a': 'apple'},
+//   {'foo': true, 'bar': false, 'baz': null},
+//   {'boolean, true': true, 'boolean, false': false, 'null': null },
+//   // basic nesting
+//   {'a': {'b': 'c'}},
+//   {'a': ['b', 'c']},
+//   [{'a': 'b'}, {'c': 'd'}],
+//   {'a': [], 'c': {}, 'b': true}
+// ];
 
-  // handle obj
+// // used for stringifyJSON spec
+// // hint: JSON does not allow you to stringify functions or
+// // undefined values, so you should skip those key/value pairs.
+// unstringifiableValues = [
+//   {
+//     'functions': function() {},
+//     'undefined': undefined
+//   }
+// ];
+// var input = {'a': 'apple'};
+// console.log(stringifyJSON(input));
+//   // return output;
 
-  // handle string   ''+'"xx"'  'xx'
+//   // handle obj
 
-  // use + method
+//   // handle string   ''+'"xx"'  'xx'
 
-};
+//   // use + method
+
+// };
+
+// // context free grammar
